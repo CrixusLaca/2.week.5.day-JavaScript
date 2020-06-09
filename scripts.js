9dee0df069d3b33169b07a3d12454ea732302cb4
BristyanLaszlo <bristyan.laszlo@gmail.com>
2020-06-09 21:21:01 +0200

    3

---
+++ scripts.js
@@ -0,0 +1,17 @@
+//let names = ['Angela', 'Mark', 'Donatella', 'Rufallo'];
+//let names = ['Angela', 'Mark', 'Donatella', 'Rufallo', 'Cintia'];
+let names = ['László', 'Mark', 'Donatella', 'Rufallo', 'Cintia'];
+console.log(names);
+console.log(names.length);
+
+let colors = ['Red', 'yellow', 'orange', 'purple'];
+colors.push('green');
+console.log(colors);
+
+let hasManyColors = ['Grey', 'Orange', 'black', 'white'];
+hasManyColors.push('pink');
+console.log(hasManyColors);
+
+hasManyColors === true;
+console.log(hasManyColors);
+
